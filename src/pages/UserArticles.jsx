92c4f0afba10a5f4636787f1dc52aa8fac2b28c1
Filip Plass <filipplass@gmail.com>
2022-10-29 18:31:30 +0200

    ha

--- src/pages/UserArticles.jsx
+++ src/pages/UserArticles.jsx
@@ -1,10 +1,11 @@
+import profilepic from "../img/profilovka.png";
 export const UserArticles = () => {
     return (
         <>
             <header className="bg-gray-100 mb-14">
                 <div className="text-center py-8">
                     <h2 className="mb-6 text-blue text-3xl">Autor</h2>
-                    <ol className="inline-flex items-center space-x-1 md:space-x-3 ">
+                    <ol className="inline-flex flex-wrap justify-center items-center space-x-1 md:space-x-3 ">
                         <li className="inline-flex items-center">
                             <a href="/" className="text-gray-700 inline-flex items-center font-medium ">
                                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
@@ -31,8 +32,27 @@ export const UserArticles = () => {
                 </div>
             </header>
             <main className="container max-w-[1200px] mx-auto md:px-10 sm:px-8 px-6">
-                <section className="max-w-[860px]">
-                    <ul>
+                <section className="mb-10">
+                    <div className=" max-w-[860px] mx-auto my-7 bg-white border-bottom md:flex-row md:max-w-xl shadow">
+                        <div className="p-4">
+                            <img className="w-48 rounded-full mx-auto" src={profilepic} alt="Bonnie image" />
+                            <h3 className="mb-2 text-2xl font-bold tracking-tight text-blue text-center">Pan Autor</h3>
+                            <p className="text-sm text-gray-500 text-center">Visual Designer</p>
+                            <p className="text-center">
+                                Lorem ipsum sit dolor amet. Amet dolor sit. Lorem ipsum sit dolor amet. Amet dolor sit. Lorem ipsum sit dolor amet. Amet dolor sit.
+                            </p>
+                            <ul className="text-gray-700 mb-2 mt-4 mx-auto max-w-[180px]">
+                                <li className="hover:text-blue flex mb-2"><svg className="w-6 h-6 text-yellow mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg><a href="tel:666666666"> +420 735 489 521</a></li>
+                                <li className="hover:text-blue flex mb-2"><svg className="w-6 h-6 text-yellow mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h4.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg><a href="tel:666666666"> +420 735 489 521</a></li>
+                                <li className="hover:text-blue flex mb-2"><svg className="w-6 h-6 text-yellow mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg><a href="mailto:aaa@aaa.cz">zasitua@zasitua.cz</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </section>
+                <section className="">
+                <h3 className="text-center text-blue mb-7 text-2xl tracking-tight leading-none  md:text-3xl lg:text-4xl">Články vybraného autora</h3>
+                    <ul className="max-w-[860px]">
                         <li>
                             <a href="#" className="flex flex-col items-center my-7 bg-white border-bottom md:flex-row md:max-w-xl">
                                 <img className="object-cover w-full h-96 md:h-auto md:w-48" src="https://cdn.pixabay.com/photo/2022/07/16/15/34/kid-7325370_960_720.jpg" alt="" />
